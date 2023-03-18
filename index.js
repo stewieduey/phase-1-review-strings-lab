32d03fb1b0899e709d2806312405f41806370b3f
DuMark Allman <dumark.allman@yahoo.com>
2023-03-18 15:23:13 -0400

    [StewieDuey]

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+// const welcomeMessage = `Welcome to Flatbook, {currentUser};
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//Should I declare shortGreeting....but didn't I do that?
+
+//const shortGreeting = `Welcome, ${currentUser}`;
+// const shortGreeting = `Welcome, {currentUser}`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+//const shortGreeting = `Welcome, {currentUser.slice(0, 1)}!`;
